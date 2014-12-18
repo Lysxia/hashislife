cbb4f2e5f1924a77423c7df5424217b97389de89
Lysxia <li-yao.xia@ens.fr>
2014-12-18 23:05:11 +0100

    Comment quad.h and break project!

--- src/quad.h
+++ src/quad.h
@@ -1,34 +1,72 @@
 #ifndef QUAD_H
 #define QUAD_H
 
-typedef struct Quad_map Quad_map;
+typedef struct QuadList QuadList;
 
 typedef struct Quad Quad;
 
-struct InNode
-{
-  Quad     *sub[4]; // subtrees : 0:upper left,  1:upper right,
-  Quad_map *next;   //            2:bottom left, 3:bottom right
+//! List of quad elements, with task identifier
+/*! */
+struct QuadList {
+  int       ql_skip_id;
+  Quad     *ql_head;
+  QuadList *ql_tail;
 };
 
-struct Leaf
+//! Quadtree internal node
+struct QInNode
+{
+  Quad *skip;           /*!< State of the center area after `2^depth` steps */
+  QuadList *short_skip; /*! State of the center area after less than
+                          `2^depth` steps */
+  Quad *sub[4];
+  /*!< Subtrees,
+    layout:
+
+        0 1
+        2 3
+  */
+};
+
+//! Quadtree leaf
+struct QLeaf
 {
   int map[4];
-  // 0 1
-  // 2 3
+  /*!< 2x2 square map,
+    layout:
+
+        0 1
+        2 3
+  */
 };
 
+//! Quadtree leaf or node
 union Node
 {
-  struct InNode n;
-  struct Leaf l;
+  struct QInNode n;
+  struct QLeaf l;
 };
 
+//! Quadtree node
+/*! A quadtree of depth `2^depth+1` represents a square area
+  with a side length of `2^(depth+1)` cells.
+                                                   
+  A leaf has depth 0;
+  an internal node has depth `depth > 0`.
+
+  ---
+
+  For an internal node, the state of the center area after exactly
+  `2^depth` steps is to be stored at `.node.n.skip`.
+
+  For inferior numbers of steps, the result is to be stored in
+  `.node.n.short_skip`.
+*/
 struct Quad
 {
-  int         depth;      // quad tree for a square map with side 2^(depth+1)
-  BigInt     *cell_count;
-  union Node  node;
+  int         depth;      
+  union Node  node;       
+  BigInt     *cell_count; /*!< Number of cells */
 };
 
 #endif
