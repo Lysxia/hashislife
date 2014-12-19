16d88b8cff41ddfa24db065fb8228bed5b4b0ab5
Lysxia <li-yao.xia@ens.fr>
2014-12-19 17:05:53 +0100

    Update comments in definitions.h

--- src/definitions.h
+++ src/definitions.h
@@ -1,8 +1,13 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
-
 #include <stdint.h>
+/*! \defgroup rules Game rules */
+/*!@{*/
+//! Rule set encoded as a bit pattern
+/*! 18 bits are used to indicate Born/Stay transitions */
+/*! \todo Explain */
 typedef uint32_t rule; // at least 18 bits
+/*!@}*/
 
 int itoa(char *dest, int src);
 
