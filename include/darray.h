0c6ef85d264cd03d432816bc698516c75df71636
Lysxia <li-yao.xia@ens.fr>
2014-12-24 14:34:02 +0100

    Update darray
    Rename Darray to DArray
    
    More error-handling-friendly interface.
    
    Error codes should not be mixed with returned values (unless sometimes
    when they are positive ints... maybe).
    E.g. if we had a signature
    
        DArray *da_new(...);
    
    since da_new(...) could fail, we would for example return NULL on
    failure (e.g., `malloc()` does this). However, in this case,
    there is a burden of allocating memory (and of freeing it) for
    something (DArray) whose size is known at compile time; and anyway
    isn't going to live long for the usage we make of it in this project.
    
    I would have originally prefered
    
        DArray da_new(...);
    
    but then in case of error we would need to return some dummy yet
    recognizable value; this overloads the returned type, which would not
    be feasible if all possible values already had meaningful semantics.
    (pleonasm!) This strategy lacks generality and even clarity in some
    (most?) cases.
    
    The solution, which I had somehow originally discarded for reasons
    now forgotten (hypothesis: laziness, unfounded efficiency concerns)
    is precisely to separate status reporting and work.
    
    Incoming commits will
    - Fix incompatibilities newly introduced by these changes
    - Modify other functions with similar issues

--- include/darray.h
+++ include/darray.h
@@ -3,16 +3,28 @@
 
 #include <stdlib.h>
 
-/* (Generic) dynamic array implementation. (Well, as generic as C allows) */
+/*! \defgroup darray (Generic) dynamic array implementation. (Well, as generic as C allows) */
 // Implements a stack
 // push elements, then recover the array of pushed elements
+/*!@{*/
+typedef struct {
+  char   *array;
+  size_t  data_size;
+  size_t  array_length;
+  size_t  max_length;
+} DArray;
 
-typedef struct Darray Darray;
+//! Initialize empty DArray
+int   da_init(size_t data_size, DArray *da);
+//! Extend the array by one uninitialized block and returns a pointer to it
+void *da_alloc(DArray *da);
+//! Extend the array and initialize it with the value pointed to by `v`
+void *da_push(DArray *da, void *v);
+//! Extract the underlying array
+void *da_unpack(DArray *da, size_t *length);
+/*! One line wrapper around `free()` */
+void  da_destroy(DArray *da);
 
-Darray *da_new(size_t data_size);
-void   *da_alloc(Darray *da);
-void   *da_push(Darray *da, void *v);
-void   *da_unpack(Darray *da, int *length);
-void    da_destroy(Darray *da);
-
+// Might need a da_append/concat some day
+/*!@}*/
 #endif
--- src/darray.c
+++ src/darray.c
@@ -3,73 +3,75 @@
 #include <string.h>
 #include "darray.h"
 
-struct Darray {
-  char   *da;
-  size_t  data_size;
-  int     array_length;
-};
+/*! Allocates a starting array.
 
-Darray *da_new(size_t data_size)
+  \param data_size Size of objects to be stored in the array.
+                   Must be positive.
+  \param da Pointer to the structure to be initialized.
+
+  Return 0 if successful, 1 otherwise. */
+int da_init(size_t data_size, DArray *da)
 {
-  Darray *da = malloc(sizeof(Darray));
-
-  if ( da == NULL )
-  {
-    perror("da_new()");
-    exit(1);
-  }
-
-  da->da = NULL;
+  const size_t da_init_max_len = 8;
   da->data_size = data_size;
   da->array_length = 0;
+  da->max_length = da_init_max_len;
+  da->array = malloc(da_init_max_len * data_size);
 
-  return da;
+  return ( NULL == da->array );
 }
 
-void *da_alloc(Darray *da)
+/*! Assumes `da->array` has already been allocated
+  (as is done by `da_init()`).
+
+  Return NULL in case of failure. */
+void *da_alloc(DArray *da)
 {
-  if ( da->array_length == 0 )
-    da->da = malloc(da->data_size);
-  else if ( !(da->array_length & (da->array_length - 1)) )
+  if ( da->array_length == da->max_length )
   {
-    da->da = realloc(da->da, 2 * da->array_length * da->data_size);
-    if ( da->da == NULL )
-    {
-      perror("da_append()");
-      exit(1);
-    }
+    da->max_length *= 2;
+    da->array = realloc(da->array, da->max_length * da->data_size);
+    if ( NULL == da->array )
+      return NULL;
   }
-  return da->da + da->data_size * da->array_length++;
+  return da->array + da->data_size * da->array_length++;
 }
 
-void *da_push(Darray *da, void *v)
+/*! Assumes `da->array` has already been allocated
+  (as is done by `da_init()`).
+
+  \param v Pointer to an object of size `da->data_size`.
+
+  Return NULL in case of failure. */
+void *da_push(DArray *da, void *v)
 {
   char *dest = da_alloc(da);
+  if ( NULL == dest )
+    return NULL;
   return memcpy(dest, (char *) v, da->data_size);
 }
 
-void *da_unpack(Darray *da, int *length)
-{
-  char *a = NULL;
+/*!
+  \param length Pointer to a location where to store the length.
+                Possibly NULL, in which case the value is just forgotten.
+                (E.g. if you just need a null-terminated string.)
 
-  if ( da->array_length > 0 )
-  {
-    a = realloc(da->da, da->array_length * da->data_size);
-    if ( a == NULL )
-    {
-      perror("da_unpack()");
-      exit(1);
-    }
-  }
-  
+  The array is resized to the given length.
+  Return NULL on failure. (That should happen only with very odd
+  implementations of `realloc()` though...
+  */
+void *da_unpack(
+  DArray *da,
+  size_t *length)
+{
   if ( NULL != length )
     *length = da->array_length;
-
-  free(da);
-  return a;
+  return realloc(da->array, da->array_length * da->data_size);
 }
 
-void da_destroy(Darray *da)
+/*! Free the array */
+void da_destroy(DArray *da)
 {
-  free(da_unpack(da, NULL));
+  free(da->array);
 }
+
